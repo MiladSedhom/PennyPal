df4d019aa7e08ea6f67598b28a9de4d4b9f7adad
milad-sedhom <miladsedhom@gmail.com>
2024-03-12 18:59:07 +0200

    add /table

---
+++ src/routes/app/table/+page.svelte
@@ -0,0 +1,135 @@
+<script lang="ts">
+	import TableCell from './TableCell.svelte'
+	import { page } from '$app/stores'
+
+	$: payments = $page.data.payments
+
+	// @ts-ignore
+	$: days = getDayInRange($page.url.searchParams.get('startDate'), $page.url.searchParams.get('endDate'))
+	let shownTags = $page.url.searchParams.get('tags')?.split(',') || $page.data.tags
+
+	console.log($page.data)
+	console.log(shownTags)
+
+	let table: any = {}
+	$: {
+		days.forEach((day) => {
+			// @ts-ignore
+			table[day] = {}
+			shownTags.forEach((tag: string) => {
+				table[tag] = {}
+				// @ts-ignore
+				table[day][tag] = sumPayments(filterPayments(payments, { tag, day }))
+				// @ts-ignore
+				table[tag].tagSum = sumPayments(filterPayments(payments, { tag }))
+			})
+			// @ts-ignore
+			table[day].dailySum = sumPayments(filterPayments(payments, { day }))
+		})
+		table.sum = sumPayments(payments)
+	}
+
+	//functions
+	function getDayInRange(startDate: string | number | Date, endDate: string | number | Date) {
+		const days: (string | number | Date)[] = []
+		for (var d = new Date(startDate); new Date(startDate) <= d && d <= new Date(endDate); d.setDate(d.getDate() + 1)) {
+			days.push(d.toLocaleDateString())
+		}
+		return days
+	}
+
+	function filterPayments(payments: any, { tag = null, day = null }) {
+		if (tag) payments = payments.filter((payment: any) => payment.tags.includes(tag))
+		if (day)
+			payments = payments.filter(
+				(payment: any) => new Date(payment.createdAt).toDateString() === new Date(day).toDateString()
+			)
+		return payments
+	}
+
+	function sumPayments(payments: any) {
+		return payments.reduce((total: any, curr: any) => total + curr.amount, 0)
+	}
+</script>
+
+<div class="container">
+	<table>
+		<tr class="title-row">
+			<th>Date</th>
+			{#each shownTags as tag}
+				<th>{tag}</th>
+			{/each}
+			<th>Daily sum</th>
+		</tr>
+		{#each days as day}
+			<tr
+				><th>{day}</th>
+				{#each shownTags as tag}
+					<TableCell --_color-percentage={`${(table[String(day)][tag] * 100) / table.sum}%`}
+						>{table[String(day)][tag]}</TableCell
+					>
+				{/each}
+				<TableCell --_color-percentage={`${(table[String(day)].dailySum * 100) / table.sum}%`}
+					>{table[String(day)].dailySum}</TableCell
+				>
+			</tr>
+		{/each}
+		<th>Tag sum</th>
+		{#each shownTags as tag}
+			<TableCell --_color-percentage={`${(table[tag].tagSum * 100) / table.sum}%`}>{table[tag].tagSum}</TableCell>
+		{/each}
+		<TableCell --_color-percentage="100%">{table.sum}</TableCell>
+	</table>
+</div>
+
+<style>
+	.container {
+		min-height: 100%;
+		width: 100%;
+		color: var(--color-text-alt);
+		padding: 2rem;
+		display: flex;
+		justify-content: start;
+		align-items: start;
+	}
+
+	table {
+		border-collapse: collapse;
+		font-size: var(--fs-base);
+		width: fit-content;
+	}
+
+	th {
+		width: 5rem;
+		padding: 0.4rem 1rem;
+		white-space: nowrap;
+		/* border: solid 1px var(--color-grey-60); */
+		text-align: center;
+		border: 1px solid var(--color-dark-1);
+	}
+
+	.title-row {
+		position: sticky;
+		top: -1px;
+
+		& th {
+			padding: 0.6rem 1rem;
+		}
+	}
+
+	:global(tr:hover td) {
+		filter: contrast(0.8);
+		/* background-color: color-mix(in srgb, black 50%, transparent); */
+	}
+
+	th {
+		font-weight: 500;
+		background-color: var(--color-dark-1);
+		color: color-mix(in srgb, var(--color-text-alt) 90, transparent);
+	}
+
+	td {
+		background-color: color-mix(in srgb, var(--color-dark), var(--color-primary) var(--_color-percentage, 0));
+		color: color-mix(in srgb, var(--color-text-alt) 90%, transparent);
+	}
+</style>
---
+++ src/routes/app/table/TableCell.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	// @ts-ignore
+	import tinycolor from 'tinycolor2'
+	import { onMount } from 'svelte'
+
+	let cellRef: any
+	let textColor: any
+
+	onMount(async () => {
+		const styles = getComputedStyle(cellRef)
+		const srgbColor = formatSrgbColor(styles.backgroundColor)
+		const color = tinycolor(srgbToRgb(srgbColor))
+		textColor = color.getLuminance() > 0.3 ? '#000' : '#fff'
+	})
+
+	function formatSrgbColor(srgbColor: string): string {
+		// Use a regular expression to extract the numeric values
+		const match = srgbColor.match(/srgb\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)/)
+
+		if (match) {
+			const r = parseFloat(match[1])
+			const g = parseFloat(match[2])
+			const b = parseFloat(match[3])
+
+			// Use template literals to format the result
+			return `srgb(${r}, ${g}, ${b})`
+		}
+
+		// Return the input as is if it doesn't match the expected format
+		return srgbColor
+	}
+
+	function srgbToRgb(srgbColor: string): string {
+		const match = srgbColor.match(/srgb\(([^,]+),\s([^,]+),\s([^)]+)\)/)
+
+		if (match) {
+			const r = parseFloat(match[1]) * 255
+			const g = parseFloat(match[2]) * 255
+			const b = parseFloat(match[3]) * 255
+
+			return `rgb(${r}, ${g}, ${b}`
+		}
+
+		// Return the input as is if it doesn't match the expected format
+		return srgbColor
+	}
+</script>
+
+<td bind:this={cellRef} style="color: {textColor};">
+	<slot />
+</td>
+
+<style>
+	td {
+		width: 5rem;
+		padding: 0.4rem 1rem;
+		white-space: nowrap;
+		/* border: solid 1px var(--color-grey-60); */
+		text-align: center;
+		border: 1px solid var(--color-dark-1);
+
+		background-color: color-mix(in srgb, var(--color-dark), var(--color-primary) var(--_color-percentage, 0));
+		color: color-mix(in srgb, var(--color-text-alt) 90%, var(--color-text-on-primary));
+	}
+</style>
