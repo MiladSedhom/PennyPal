592d037ac63dda36220eba47863961bb4363aaf2
milad-sedhom <miladsedhom@gmail.com>
2024-03-12 12:28:06 +0200

    feat: perserve date input state, and properly reset the Selects state

--- src/routes/app/(components)/AddPaymentForm.svelte
+++ src/routes/app/(components)/AddPaymentForm.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import { enhance } from '$app/forms'
+	import type { SubmitFunction } from '../$types'
 	import { page } from '$app/stores'
 	import Select from '$lib/components/Select.svelte'
 
 	let selectedTags: string[]
 	const options = $page.data.tags.map((o: string) => ({ label: o, value: o }))
+
+	let dateInput: HTMLDataElement
+	const submitter: SubmitFunction = () => {
+		const dateValue = dateInput.value
+		return async ({ update }) => {
+			await update()
+			// keeping the date, cuz the enchace action resest is
+			dateInput.value = dateValue
+			// when the form is reset it does not show on the select element so i have to do it myself, can also alterntivly keep its state
+			selectedTags = []
+		}
+	}
 </script>
 
-<form action="?/addPayment" method="post" use:enhance>
+<form action="?/addPayment" method="post" use:enhance={submitter}>
 	<div class="field">
 		<label for="amount">Amount</label>
 		<input type="number" name="amount" id="amount" placeholder="amount" />
@@ -26,7 +39,7 @@
 
 	<div class="field">
 		<label for="date">Date</label>
-		<input type="date" name="date" id="date" value={new Date().toISOString().substring(0, 10)} />
+		<input bind:this={dateInput} type="date" name="date" id="date" value={new Date().toISOString().substring(0, 10)} />
 		{#if $page.form?.errors?.date}
 			<div class="error">{$page.form.errors.date}</div>
 		{/if}
