c14620fafdf63df87bcea3c400184121c657441f
MiladSedhom <MiladSedhom@gmail.com>
2024-07-24 22:49:49 +0300

    add colors

--- uno.config.ts
+++ uno.config.ts
@@ -20,7 +20,10 @@ export default defineConfig({
 			fields: 'var(--color-fields)',
 			bg: 'var(--color-background)',
 			bg1: 'var(--color-background-1)',
-			bg2: 'var(--color-background-2)'
+			bg2: 'var(--color-background-2)',
+			'text-90': 'var(--color-text-90)',
+			'text-70': 'var(--color-text-70)',
+			'text-60': 'var(--color-text-60)'
 		}
 	}
 })
