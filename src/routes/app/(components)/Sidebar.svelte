7e200e6824777abd11828204f92a8078a14453fe
MiladSedhom <MiladSedhom@gmail.com>
2024-08-03 03:23:51 +0300

    feat: add dropdown in sidebar

--- src/routes/app/(components)/Sidebar.svelte
+++ src/routes/app/(components)/Sidebar.svelte
@@ -4,7 +4,13 @@
 	import FiltersForm from './FiltersForm.svelte'
 	import Modal from '$lib/components/Modal.svelte'
 	import ThemeSwitchers from '$lib/components/ThemeSwitchers.svelte'
+	import { DropdownMenu } from 'bits-ui'
 	import { slide } from 'svelte/transition'
+	import { themeSwitcher } from '$lib/themeSwitcher/themeSwitcher'
+	import Dropdown from '$lib/components/Dropdown.svelte'
+
+	const theme = themeSwitcher('theme')
+	const color = themeSwitcher('color', 'green')
 
 	let isFilters: boolean
 	let isAdd: boolean
@@ -18,34 +24,89 @@
 					<span class="text-primary">Penny</span>Pal
 				</a>
 			</span>
-			<ThemeSwitchers />
-			<div class="sm:hidden">
-				<button
-					on:click={() => {
-						isFilters = !isFilters
-					}}
-				>
-					filters
-				</button>
-				<button
-					on:click={() => {
-						isAdd = true
-					}}
-				>
-					add
-				</button>
-			</div>
-			<div class="">
+			<div class="flex items-center gap-2">
 				<p class="text-3 text-text-90">{$page.data.user.username}</p>
-				<a href="/logout" class="text-3 text-text-70">logout</a>
+
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger class="hover:bg-muted rounded-1 p-1">
+						<div class="i-tabler-dots-vertical text-5"></div>
+					</DropdownMenu.Trigger>
+
+					<DropdownMenu.Content class="bg-bg border-border text-3 rounded-1 w-60 border p-1">
+						<DropdownMenu.Group class="sm:hidden">
+							<DropdownMenu.Item
+								class="text-text hover:bg-muted flex h-10 items-center p-2"
+								on:click={() => {
+									isFilters = !isFilters
+								}}
+							>
+								<div class="i-tabler-filter text-5 bg-text-90 m-r-2"></div>
+								Filters
+							</DropdownMenu.Item>
+
+							<DropdownMenu.Item
+								class="text-text hover:bg-muted flex h-10 items-center p-2"
+								on:click={() => {
+									isAdd = true
+								}}
+							>
+								<div class="i-tabler-circle-plus text-5 bg-text-90 m-r-2"></div>
+								Add a New Payment
+							</DropdownMenu.Item>
+							<DropdownMenu.Separator class="bg-border h-1px w-80% m-a " />
+						</DropdownMenu.Group>
+						<DropdownMenu.Group>
+							<DropdownMenu.Item
+								class="text-text hover:bg-muted flex h-10 items-center p-2"
+								on:click={() => {
+									$theme = $theme === 'dark' ? 'light' : 'dark'
+								}}
+							>
+								<div class="i-tabler-moon-stars text-5 bg-text-90 m-r-2"></div>
+								Change Theme
+							</DropdownMenu.Item>
+
+							<DropdownMenu.Item
+								class="text-text  hover:bg-muted flex h-10 items-center p-2"
+								on:click={() => {
+									const colors = ['green', 'blue', 'orange', 'pink']
+									const newColorIndex = $color ? (colors.indexOf($color) + 1) % colors.length : 0
+									$color = colors.at(newColorIndex)
+								}}
+							>
+								<div class="i-tabler-color-filter text-5 bg-text-90 m-r-2"></div>
+								Change Color
+							</DropdownMenu.Item>
+						</DropdownMenu.Group>
+						<DropdownMenu.Separator class="bg-border h-1px w-80% m-a " />
+
+						<DropdownMenu.Item class="text-text  hover:bg-muted  h-10 p-2">
+							<a href="/logout" class="flex size-full items-center">
+								<div
+									class="i-tabler-logout-2 text-5 bg-text-90
+bg-text-90 m-r-2"
+								></div>
+								Log Out</a
+							>
+						</DropdownMenu.Item>
+
+						<DropdownMenu.Sub>
+							<DropdownMenu.SubTrigger />
+							<DropdownMenu.SubContent />
+						</DropdownMenu.Sub>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
 			</div>
 		</div>
 
 		{#key isFilters}
-			<div class=" m-b-4 grow {isFilters || 'hidden'} sm:block" transition:slide={{ duration: 300 }}>
-				<h3 class="text-4 fw-400 m-t-4 m-b-2">Choose Your Filters</h3>
+			<div class=" m-block-4 grow {isFilters || 'hidden'} sm:block" transition:slide={{ duration: 350 }}>
+				<h3 class="text-14px fw-400 m-t-4 m-b-2">Choose Your Filters</h3>
 
 				<FiltersForm />
+				<button class=" h-8 w-full sm:hidden" on:click={() => (isFilters = !isFilters)}>
+					<div class="i-tabler-caret-up text-6 m-a"></div>
+				</button>
 			</div>
 		{/key}
 		<button
@@ -58,20 +119,18 @@
 			<div class="i-tabler-plus text-5 m-l-2"></div>
 		</button>
 
-		{#if isAdd}
-			<Modal
-				showModal
-				onClose={() => {
+		<Modal
+			showModal={isAdd}
+			onClose={() => {
+				isAdd = false
+			}}
+		>
+			<AddPaymentForm
+				onSubmit={() => {
 					isAdd = false
 				}}
-			>
-				<AddPaymentForm
-					onSubmit={() => {
-						isAdd = false
-					}}
-				/>
-			</Modal>
-		{/if}
+			/>
+		</Modal>
 	</aside>
 
 	<nav class="m-inline-auto bg-bg w-full">
