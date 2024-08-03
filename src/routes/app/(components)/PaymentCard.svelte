e82c34b9ec14c5d0d70b63b84d471767cde4812d
MiladSedhom <MiladSedhom@gmail.com>
2024-08-04 02:41:17 +0300

    payment card redesign

--- src/routes/app/(components)/PaymentCard.svelte
+++ src/routes/app/(components)/PaymentCard.svelte
@@ -9,20 +9,27 @@
 </script>
 
 <div
-	class="bg-bg border-rd-1 flex min-w-80 flex-col gap-4 p-8
+	class="bg-primary text-text-alt border-rd-1 flex min-w-80 flex-col gap-4 p-6
 	{selected && 'outline-color-primary outline-solid outline-2'}"
 	on:click
-	on:dblclick={() => {
-		goto(`/app/payment/${payment.id}`)
-	}}
 	on:keydown
 	role="button"
 	tabindex="0"
 	transition:fade={{ duration: 200 }}
 >
-	<div class="flex basis-full justify-between">
-		<span class="text-6 fw-500">{payment.amount}</span>
+	<div class="flex basis-full items-start justify-between">
+		<span class="text-10 fw-700 font-[var(--serif)]">{payment.amount}</span>
 
+		<div class="p-t-2 flex flex-wrap justify-end gap-2">
+			{#each payment.tags as tag}
+				<span class="bg-text text-14px text-text-alt p-x-4 p-y-1 rounded-1 font-700"> {tag} </span>
+			{/each}
+		</div>
+	</div>
+	{#if payment.note}
+		<span class="text-3 fw-500 p-r-2 grow" title={payment.note}>{payment.note}</span>
+	{/if}
+	<div class="flex grow items-center justify-between">
 		<div class="flex gap-1">
 			<form action="?/removePayment" method="post" use:enhance>
 				<input type="hidden" name="id" value={payment.id} />
@@ -38,17 +45,6 @@
 				</a>
 			</form>
 		</div>
+		<span class="text-3 fw-600 self-end">{new Date(payment.createdAt).toLocaleDateString()}</span>
 	</div>
-	<div class="h-4"></div>
-	<div class="flex grow items-center justify-between">
-		<div class="flex flex-wrap gap-1">
-			{#each payment.tags as tag}
-				<span class="bg-primary text-12px text-text-alt p-x-4 p-y-1 border-rd-1"> {tag} </span>
-			{/each}
-		</div>
-		<span class="text-3">{new Date(payment.createdAt).toLocaleDateString()}</span>
-	</div>
-	{#if payment.note}
-		<span class="text-3 fw-400 grow" title={payment.note}>{payment.note ? payment.note : ''}</span>
-	{/if}
 </div>
