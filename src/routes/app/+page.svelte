9b703bbe17b603689eac461829c56f4697bece40
MiladSedhom <MiladSedhom@gmail.com>
2024-08-05 00:51:53 +0300

    feat: masonry layout

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,43 +1,66 @@
 <script lang="ts">
 	import PaymentCard from './(components)/PaymentCard.svelte'
 	import Infobar from './(components)/Infobar.svelte'
-	import { flip } from 'svelte/animate'
-	import { quintOut } from 'svelte/easing'
+	// @ts-ignore
+	import Masonry from 'masonry-layout'
+	import { onMount, afterUpdate } from 'svelte'
+
 	export let data
 
 	let selectedPayments = new Set<number>()
 
-	const handleOnClick = (id: number) => {
+	const handleClick = (id: number) => {
 		if (selectedPayments.has(id)) selectedPayments.delete(id)
 		else selectedPayments.add(id)
 		selectedPayments = selectedPayments
 	}
+
+	let masonry: any
+
+	onMount(() => {
+		masonry = new Masonry('.masonry', {
+			itemSelector: '.masonry-item',
+			columnWidth: '.masonry-item',
+			gutter: 16,
+			horizontalOrder: true,
+			fitWidth: true
+		})
+
+		return () => {
+			masonry.destroy()
+		}
+	})
+
+	afterUpdate(() => {
+		masonry.reloadItems()
+		masonry.layout()
+	})
 </script>
 
-<div
-	class="m-b-8 grid min-h-full w-full grid-cols-[repeat(auto-fill,320px)] grid-rows-[repeat(auto-fill,minmax(16px,auto))] place-content-center gap-4 p-8"
->
-	{#if data.payments?.length}
-		{#each data.payments as payment (payment.id)}
-			<div animate:flip={{ duration: 500, easing: quintOut }}>
-				<PaymentCard
-					{payment}
-					selected={selectedPayments.has(payment.id)}
-					on:click={() => {
-						handleOnClick(payment.id)
-					}}
-					on:keydown={(event) => {
-						if (event.code === 'Enter' || event.code === 'Space') handleOnClick(payment.id)
-					}}
-				/>
-			</div>
-		{/each}
-	{/if}
-</div>
-<div class="pos-fixed bottom-0 w-full">
-	{#if data.payments?.length}
-		<Infobar
-			payments={selectedPayments.size === 0 ? data.payments : data.payments.filter((p) => selectedPayments.has(p.id))}
-		/>
-	{/if}
+<div class="size-full p-6">
+	<div class="masonry m-x-auto">
+		{#if data.payments?.length}
+			{#each data.payments as payment (payment.id)}
+				<div class="masonry-item m-b-4 max-w-80">
+					<PaymentCard
+						{payment}
+						selected={selectedPayments.has(payment.id)}
+						on:click={() => {
+							handleClick(payment.id)
+						}}
+						on:keydown={(event) => {
+							if (event.code === 'Enter' || event.code === 'Space') handleClick(payment.id)
+						}}
+					/>
+				</div>
+			{/each}
+		{/if}
+	</div>
+	<div class="pos-fixed bottom-0 w-full">
+		{#if data.payments?.length}
+			<Infobar
+				payments={selectedPayments.size === 0 ? data.payments : data.payments.filter((p) => selectedPayments.has(p.id))}
+			/>
+		{/if}
+	</div>
 </div>
