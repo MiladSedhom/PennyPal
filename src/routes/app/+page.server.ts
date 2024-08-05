8f35f96635fcf2062e43539b9589e5af4ce54f1a
MiladSedhom <MiladSedhom@gmail.com>
2024-08-05 15:11:49 +0300

    move update payment action

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,6 +1,6 @@
 export const ssr = false
 
-import { addPayment, removePayment } from '$lib/server/database'
+import { addPayment, removePayment, updatePayment } from '$lib/server/database'
 import { fail } from '@sveltejs/kit'
 import type { Actions } from './$types'
 import { paymentSchema } from '../../lib/zodSchemas'
@@ -39,6 +39,29 @@ export const actions: Actions = {
 			await removePayment(id, locals.user?.id)
 		} catch (e) {
 			console.log(e)
+		}
+	},
+	updatePayment: async ({ request, locals }) => {
+		const formData = await request.formData()
+
+		//validate from data
+		const result = paymentSchema.safeParse(formData)
+
+		if (!result.success) {
+			return fail(400, {
+				data: Object.fromEntries(formData),
+				errors: result.error.flatten().fieldErrors
+			})
+		}
+
+		//query
+		try {
+			// @ts-ignore
+			await updatePayment({ ...result.data }, locals.user.id)
+			return { success: true }
+		} catch (error) {
+			console.log(error)
+			fail(500, { error })
 		}
 	}
 }
