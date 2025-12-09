8cd4f24b10cb8d14b4c6353f4ec53de759c6eda3
Milad Sedhom <MiladSedhom@gmail.com>
2025-12-09 23:47:19 +0200

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="bg-gray-800 text-emerald-50 p-4 w-screen h-screen">
+	<h1>Welcome to SvelteKit From Zed.</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
