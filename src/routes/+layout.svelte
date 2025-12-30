4cca0e98b5f3237887689bbcfa052613658c6516
Milad Sedhom <MiladSedhom@gmail.com>
2025-12-30 13:23:07 +0200

    feat: extract css slide-in and slide-out with rotations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,10 +32,8 @@
 				</form>
 			{/if}
 			<Button onclick={toggleMode} variant="outline" size="icon">
-				<SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90" />
-				<MoonIcon
-					class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
-				/>
+				<SunIcon class="h-[1.2rem] w-[1.2rem] scale-in dark:scale-out-reverse " />
+				<MoonIcon class="absolute h-[1.2rem] w-[1.2rem] scale-out dark:scale-in " />
 				<span class="sr-only">Toggle theme</span>
 			</Button>
 		</div>
