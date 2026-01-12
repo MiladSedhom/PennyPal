6ec4d56132dbb7c5dbb8222ef2346148065fdb21
Milad Sedhom <MiladSedhom@gmail.com>
2026-01-13 01:50:11 +0200

    wip: payment form inputs grid

--- src/lib/components/payments-form.svelte
+++ src/lib/components/payments-form.svelte
@@ -20,12 +20,15 @@
 	<ul class="flex flex-col gap-6">
 		{#each paymentsForms as p, index}
 			<li>
-				<form class="gap-2 flex flex-col md:flex-row items-start">
-					<Input bind:value={p.amount} class="min-w-30 md:max-w-30" type="number" placeholder="amount" />
-					<DatePicker bind:value={p.date} class="bg-amber-400 min-w-50 w-full md:max-w-50" />
+				<form
+					class="grid grid-rows-3 grid-cols-[6rem_1fr] gap-2 sm:grid-rows-3 sm:grid-cols-[6rem_repeat(2,1fr)] md:grid-cols-[6rem_repeat(3,1fr)_auto] md:grid-rows-1 max-w-180"
+				>
+					<Input bind:value={p.amount} type="number" placeholder="amount" />
+
+					<DatePicker bind:value={p.date} class="bg-amber-400 w-full" />
 
 					<Select.Root type="multiple" onValueChange={(values) => (p.tags = values.map((v) => parseInt(v)))}>
-						<Select.Trigger class="min-w-50 w-full md:max-w-50">
+						<Select.Trigger class="w-full max-sm:col-span-2">
 							{@const tagNames = tags
 								.filter((t) => p.tags.includes(t.id))
 								.map((t) => t.name)
@@ -41,9 +44,48 @@
 						</Select.Content>
 					</Select.Root>
 
-					<Textarea bind:value={p.note} class="min-h-9 py-1 md:max-w-50" placeholder="note" rows={1} />
+					<div class="max-sm:col-span-2 max-md:col-span-3 flex gap-2 md:contents">
+						<Textarea bind:value={p.note} class="min-h-9 py-1 " placeholder="Note..." rows={1} />
+
+						{#if index > 0 || true}
+							<Button variant="destructive" type="button" size="icon" onclick={() => paymentsForms.splice(index, 1)}>
+								<XIcon />
+							</Button>
+						{/if}
+					</div>
+				</form>
+				<form class="flex flex-wrap max-w-174 *:basis-48 mt-10 gap-2 *:grow">
+					<Input bind:value={p.amount} capture class="sm:max-w-24" type="number" placeholder="amount" />
+
+					<DatePicker bind:value={p.date} class="bg-amber-400  " />
+
+					<Select.Root type="multiple" onValueChange={(values) => (p.tags = values.map((v) => parseInt(v)))}>
+						<Select.Trigger class="w-full ">
+							{@const tagNames = tags
+								.filter((t) => p.tags.includes(t.id))
+								.map((t) => t.name)
+								.join(', ')}
+							<span class="truncate" title={tagNames}>
+								{p.tags.length === 0 ? 'Select tags...' : `${tagNames}`}
+							</span>
+						</Select.Trigger>
+						<Select.Content>
+							{#each tags as tag}
+								<Select.Item value={tag.id.toString()}>{tag.name}</Select.Item>
+							{/each}
+						</Select.Content>
+					</Select.Root>
+
+					<Textarea bind:value={p.note} class="min-h-9 py-1 " placeholder="Note..." rows={1} />
+
 					{#if index > 0}
-						<Button variant="destructive" type="button" size="icon" onclick={() => paymentsForms.splice(index, 1)}>
+						<Button
+							class="sm:max-md:col-span-2 max-md:max-w-full max-md:w-full md:col-span-1"
+							variant="destructive"
+							type="button"
+							size="icon"
+							onclick={() => paymentsForms.splice(index, 1)}
+						>
 							<XIcon />
 						</Button>
 					{/if}
