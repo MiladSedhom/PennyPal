cd3f8e9705fd5185f93d85dffd24bdee08afa23d
Milad Sedhom <MiladSedhom@gmail.com>
2026-01-14 00:34:41 +0200

    wip: payment form

--- src/lib/components/payments-form.svelte
+++ src/lib/components/payments-form.svelte
@@ -8,115 +8,104 @@
 	import DatePicker from '$lib/components/ui/date-picker/date-picker.svelte'
 	import { type DateValue, getLocalTimeZone, today } from '@internationalized/date'
 	import * as Select from '$lib/components/ui/select/index.js'
+	import * as Dialog from '$lib/components/ui/dialog'
+
+	let open = $state(true)
 
 	const paymentsForms = $state<{ amount: number; tags: number[]; note: string; date: DateValue }[]>([
+		{ amount: 0, tags: [], note: '', date: today(getLocalTimeZone()) },
 		{ amount: 0, tags: [], note: '', date: today(getLocalTimeZone()) }
 	])
 
 	const tags = $derived(await getTags())
 </script>
 
-<div class="p-4 border-2 rounded-l space-y-8">
-	<ul class="flex flex-col gap-6">
-		{#each paymentsForms as p, index}
-			<li>
-				<form
-					class="grid grid-rows-3 grid-cols-[6rem_1fr] gap-2 sm:grid-rows-3 sm:grid-cols-[6rem_repeat(2,1fr)] md:grid-cols-[6rem_repeat(3,1fr)_auto] md:grid-rows-1 max-w-180"
-				>
-					<Input bind:value={p.amount} type="number" placeholder="amount" />
+<svelte:window
+	on:keydown={(e) => {
+		if (e.key === 'n' && e.altKey) {
+			open = !open
+			e.preventDefault()
+		}
+	}}
+/>
 
-					<DatePicker bind:value={p.date} class="bg-amber-400 w-full" />
+<Dialog.Root bind:open>
+	<Dialog.Content class=" min-h-[45%] h-[50%] overflow-y-scroll flex flex-col justify-between gap-8">
+		<Dialog.Header class="w-full max-h-12 stikcy top-0">
+			<Dialog.Title class="text-lg font-semibold text-start w-full">Add Payments</Dialog.Title>
+		</Dialog.Header>
 
-					<Select.Root type="multiple" onValueChange={(values) => (p.tags = values.map((v) => parseInt(v)))}>
-						<Select.Trigger class="w-full max-sm:col-span-2">
-							{@const tagNames = tags
-								.filter((t) => p.tags.includes(t.id))
-								.map((t) => t.name)
-								.join(', ')}
-							<span class="truncate" title={tagNames}>
-								{p.tags.length === 0 ? 'Select tags...' : `${tagNames}`}
-							</span>
-						</Select.Trigger>
-						<Select.Content>
-							{#each tags as tag}
-								<Select.Item value={tag.id.toString()}>{tag.name}</Select.Item>
-							{/each}
-						</Select.Content>
-					</Select.Root>
-
-					<div class="max-sm:col-span-2 max-md:col-span-3 flex gap-2 md:contents">
-						<Textarea bind:value={p.note} class="min-h-9 py-1 " placeholder="Note..." rows={1} />
-
-						{#if index > 0 || true}
-							<Button variant="destructive" type="button" size="icon" onclick={() => paymentsForms.splice(index, 1)}>
-								<XIcon />
-							</Button>
-						{/if}
-					</div>
-				</form>
-				<form class="flex flex-wrap max-w-174 *:basis-48 mt-10 gap-2 *:grow">
-					<Input bind:value={p.amount} capture class="sm:max-w-24" type="number" placeholder="amount" />
-
-					<DatePicker bind:value={p.date} class="bg-amber-400  " />
-
-					<Select.Root type="multiple" onValueChange={(values) => (p.tags = values.map((v) => parseInt(v)))}>
-						<Select.Trigger class="w-full ">
-							{@const tagNames = tags
-								.filter((t) => p.tags.includes(t.id))
-								.map((t) => t.name)
-								.join(', ')}
-							<span class="truncate" title={tagNames}>
-								{p.tags.length === 0 ? 'Select tags...' : `${tagNames}`}
-							</span>
-						</Select.Trigger>
-						<Select.Content>
-							{#each tags as tag}
-								<Select.Item value={tag.id.toString()}>{tag.name}</Select.Item>
-							{/each}
-						</Select.Content>
-					</Select.Root>
-
-					<Textarea bind:value={p.note} class="min-h-9 py-1 " placeholder="Note..." rows={1} />
-
-					{#if index > 0}
-						<Button
-							class="sm:max-md:col-span-2 max-md:max-w-full max-md:w-full md:col-span-1"
-							variant="destructive"
-							type="button"
-							size="icon"
-							onclick={() => paymentsForms.splice(index, 1)}
+		<div class="flex flex-col gap-8 items-center justify-start *:max-w-120 grow">
+			<ul class="flex flex-col gap-6">
+				{#each paymentsForms as p, index}
+					<li>
+						<form
+							class="[--cols:1] has-[#d-btn]:[--cols:2] sm:[--cols:2] sm:has-[#d-btn]:[--cols:3] md:[--cols:3] md:has-[#d-btn]:[--cols:4] grid grid-cols-1 sm:grid-cols-[6rem_1fr] md:grid-cols-[6rem_1fr_1fr] gap-2"
 						>
-							<XIcon />
-						</Button>
-					{/if}
-				</form>
-			</li>
-		{/each}
-	</ul>
+							<Input bind:value={p.amount} class="max-w-full" type="number" placeholder="amount" />
 
-	<div>
-		<Button
-			variant="default"
-			type="button"
-			onclick={() => paymentsForms.push({ amount: 0, tags: [], note: '', date: paymentsForms.at(-1)!.date })}
-		>
-			Add Payment
-		</Button>
-		<Button
-			variant="default"
-			type="button"
-			onclick={async () => {
-				await createPayments({
-					payments: paymentsForms.map((pf) => ({
-						amount: pf.amount,
-						note: pf.note,
-						tags: pf.tags,
-						date: pf.date.toDate(getLocalTimeZone())
-					}))
-				}).updates(getPayments())
-			}}
-		>
-			Submit Payments
-		</Button>
-	</div>
-</div>
+							<DatePicker bind:value={p.date} class="bg-amber-400 w-full col-span-(--cols) sm:col-span-1" />
+
+							<Select.Root type="multiple" onValueChange={(values) => (p.tags = values.map((v) => parseInt(v)))}>
+								<Select.Trigger class="w-full col-span-(--cols) md:col-span-1">
+									{@const tagNames = tags
+										.filter((t) => p.tags.includes(t.id))
+										.map((t) => t.name)
+										.join(', ')}
+									<span class="truncate" title={tagNames}>
+										{p.tags.length === 0 ? 'Select tags...' : `${tagNames}`}
+									</span>
+								</Select.Trigger>
+								<Select.Content>
+									{#each tags as tag}
+										<Select.Item value={tag.id.toString()}>{tag.name}</Select.Item>
+									{/each}
+								</Select.Content>
+							</Select.Root>
+
+							<Textarea bind:value={p.note} class="min-h-9 py-1 col-span-(--cols)" placeholder="Note..." rows={1} />
+
+							{#if index > 0}
+								<Button
+									id="d-btn"
+									class="row-1 col-(--cols)"
+									variant="destructive"
+									type="button"
+									size="icon"
+									onclick={() => paymentsForms.splice(index, 1)}
+								>
+									<XIcon />
+								</Button>
+							{/if}
+						</form>
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="flex *:grow w-full max-w-full gap-4">
+			<Button
+				variant="outline"
+				type="button"
+				onclick={() => paymentsForms.push({ amount: 0, tags: [], note: '', date: paymentsForms.at(-1)!.date })}
+			>
+				Add Payment
+			</Button>
+			<Button
+				variant="default"
+				type="button"
+				onclick={async () => {
+					await createPayments({
+						payments: paymentsForms.map((pf) => ({
+							amount: pf.amount,
+							note: pf.note,
+							tags: pf.tags,
+							date: pf.date.toDate(getLocalTimeZone())
+						}))
+					}).updates(getPayments())
+				}}
+			>
+				Submit Payments
+			</Button>
+		</div>
+	</Dialog.Content>
+</Dialog.Root>
